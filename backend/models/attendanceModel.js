9cbc99d2a35e27f28da75c1883522993de50adac
pascalion-ex <kerolos.a.fam@gmail.com>
2026-09-18 07:36:36 +0300

    attendace model

--- backend/models/attendanceModel.js
+++ backend/models/attendanceModel.js
@@ -1,1 +1,49 @@
 import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema(
+    {
+        child:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "childprofile",
+            required: true,
+        },
+        serviceDate: {
+            type: Date,
+            required: true,
+        },
+        serviceYear: {
+            type: Number,
+            required: true,
+        },status: {
+      type: String,
+      enum: ["present", "absent", "late", "excused"],
+      default: "present",
+    },
+
+    recordedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    notes: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+attendanceSchema.index(
+  {
+    child: 1,
+    serviceDate: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+export default mongoose.model("Attendance", attendanceSchema);
